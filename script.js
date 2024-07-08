0046bee9d666db069268b988e95a3d767de2371f
alexa0501 <156824760+alexa0501@users.noreply.github.com>
2024-07-08 15:08:03 +0200

    Fast Fertig

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//# sourceMappingURL=script.js.map
+
+function toggleDetails(element) {
+    const details = element.nextElementSibling;
+    const plus = element.querySelector('.plus');
+
+    if (details.style.display === 'none' || details.style.display === '') {
+        details.style.display = 'block';
+        plus.textContent = '-';
+        element.querySelector('span:first-child').textContent = 'Details';
+        element.classList.add('active');
+        details.classList.add('active');
+    } else {
+        details.style.display = 'none';
+        plus.textContent = '+';
+        element.classList.remove('active');
+        details.classList.remove('active');
+    }
+}
+
